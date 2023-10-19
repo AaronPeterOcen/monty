0ef1704957fd4edd79a268c7d8d17d9cf3b04859
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-20 00:47:33 +0300

    tasks

--- montymain.c
+++ montymain.c
@@ -16,10 +16,14 @@ int main(int argc, char **argv)
 	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
+	{
 		return (usage_error());
+	}
 	script_fd = fopen(argv[1], "r");
 	if (script_fd == NULL)
+	{
 		return (f_open_error(argv[1]));
+	}
 	exit_code = run_monty(script_fd);
 	fclose(script_fd);
 	return (exit_code);
