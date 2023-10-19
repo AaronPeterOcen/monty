8f679dc82812398fedd8ae32c9ffb3420bbe2544
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-20 00:22:22 +0300

    tasks

--- h/montyfuncs3.c
+++ h/montyfuncs3.c
@@ -1,5 +1,6 @@
 #include "monty.h"
 
+void mpchar(stack_t **stk, unsigned int ln);
 void mpstr(stack_t **stk, unsigned int ln);
 void mnop(stack_t **stk, unsigned int ln);
 
@@ -32,4 +33,26 @@ void mpstr(stack_t **stk, unsigned int ln)
 	printf("\n");
 
 	(void)ln;
+}
+
+/**
+ * mpchar - Prints the character in the top value
+ *               node of a stack_t linked list.
+ * @stk: A pointer to the top mode node of a stack_t linked list.
+ * @ln: The current working line number of a Monty bytecodes file.
+ */
+void mpchar(stack_t **stk, unsigned int ln)
+{
+	if ((*stk)->next == NULL)
+	{
+		set_op_tok_error(pchar_error(ln, "stack empty"));
+		return;
+	}
+	if ((*stk)->next->n < 0 || (*stk)->next->n > 127)
+	{
+		set_op_tok_error(pchar_error(ln, "value out of range"));
+		return;
+	}
+
+	printf("%c\n", (*stk)->next->n);
 }
